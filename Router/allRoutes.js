5dfcf98b74ecff854224468323235e29b9a7fa74
quorentin <quorentinrobichon@gmail.com>
2025-03-20 22:46:58 +0100

    gestion de laffichage reservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "la galerie", "/pages/galerie.html"),
-    
+    new Route("/signin","Connexion","/pages/auth/signin.html"),
+    new Route("/signup","inscription","/pages/auth/signup.html"),
+    new Route("/account","mon compte","/pages/auth/account.html"),
+    new Route("/editPassworld","changement de mdp","/pages/auth/editPassworld.html"),
+    new Route("/allResa","Reservation","/pages/reservations/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
